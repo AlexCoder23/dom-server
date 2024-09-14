88b127a484dd8004d8647e6b12766d403f1e3dfe
AlexCoder23 <112844632+AlexCoder23@users.noreply.github.com>
2024-09-14 10:51:03 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
-import streamlit as st
+# import os, datetime, time, sys, json, traceback, html, importlib, requests
+from quart import (Quart, redirect, url_for, session, request, jsonify, render_template, Blueprint)
+# from quart_compress import Compress
+# import socketio
+# import blueprints, api, sockets
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+HOST = 'real-dog-together.ngrok-free.app'
+  
+app = Quart(__name__)
+app.config['SECRET_KEY'] = os.environ['FLASK_SECRET']
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MAX_CONTENT_LENGTH'] = 4*1024*1024*1024
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.jinja_env.auto_reload = True
+# compress = Compress(app)
+app.permanent_session_lifetime = datetime.timedelta(days=7)
+# sio = socketio.AsyncServer(
+#   async_mode='asgi',
+# )
+
+# blueprints.load(app)
+# sockets.load(sio)
+
+@app.errorhandler(404)
+async def page_404(e):
+  return await render_template('404Page.html')
+
+# asgi = socketio.ASGIApp(sio, app)
